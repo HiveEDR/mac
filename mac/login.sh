cacf3e7c5f01cf29a0cae4f31b7f05d9c5b3b975
Protocol Soft for Software Development <info@protosoft.cloud>
2026-05-21 05:03:30 +0300

    Configure GitHub Actions workflows and local scripts

--- mac/login.sh
+++ mac/login.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Waiting for ngrok to initialize..."
+sleep 10
 echo ..........................................................
 echo IP:
 curl -s http://localhost:4040/api/tunnels | grep -o '"public_url":"[^"]*' | sed 's/"public_url":"//'
